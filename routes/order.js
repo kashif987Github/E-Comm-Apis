77425b5af657f83cb07a4610032bf02d22004476
apnacolege git demo class <kash@gmail.com>
2025-01-16 22:45:46 +0530

    updated code

--- routes/order.js
+++ routes/order.js
@@ -1,6 +1,54 @@
 const express = require('express');
 const route = express.Router();
+const orderModel = require('../models/order.js');
+const isAdmin = require('./varifyToken.js');
+ 
 
 
+
+ route.post("/save",isAdmin,async(req,res)=>{
+    const neworder=new order (req.body)
+    try{
+        const saveorder= await neworder.save()
+        res.status(200).json(saveorder);
+    }catch(err){
+        res.status(500).json(err);
+
+    }
+ })
+
+
+ route.get("/", isAdmin,async(req,res)=>{
+    try{
+     const order=  await orderModel.find()
+          res.status(200).json(order);
+      
+    }catch(err){
+     res.status(500).json(err);
+ 
+ 
+    }
+  })
+
+  
+
+route.delete("/delete", isAdmin, async(req,res)=>{
+    try{
+        
+   await orderModel.findByIdAndDelete(req.params.id)
+   res.status(404).json("order has been deleted ")
+    }catch(err){
+        res.status(500).json(err);
+
+    }
+
+    
+
+})
+
+
+ 
+
+ 
 
 module.exports = route
